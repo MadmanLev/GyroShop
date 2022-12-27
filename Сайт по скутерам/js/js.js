a4fc82616efd9ee0030caf092202ba49053e61f1
MadmanLev <107801337+MadmanLev@users.noreply.github.com>
2022-12-27 13:34:05 +0500

    Delete Сайт по скутерам directory

--- Сайт по скутерам/js/js.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.querySelector(".m-menu");
-const menu = document.querySelector(".menu");
-menuButton.addEventListener("click", () => {
-    menu.classList.toggle("is-open");
-})
